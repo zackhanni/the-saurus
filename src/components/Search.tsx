44ff6555cb0b3b84a23bd2320cb4c4d16247a29a
Zack Hanni <zack.hannI@gmail.com>
2023-08-07 11:07:14 -0400

    add search and submit button

---
+++ src/components/Search.tsx
@@ -0,0 +1,14 @@
+export default function Search() {
+    return (
+        <form>
+            <input 
+                type="text" 
+                id="search" 
+                name="search" 
+                value="Search..."
+            >
+            </input>
+            <button>Roar!</button>
+        </form>
+    )
+}
