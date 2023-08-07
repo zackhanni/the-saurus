ea05e0eed6ff9596bb2a75323c9ce6c417f9479e
Zack Hanni <zack.hannI@gmail.com>
2023-08-07 11:07:00 -0400

    add demo results for formatting

---
+++ src/components/Results.tsx
@@ -0,0 +1,16 @@
+export default function Results() {
+    return (
+        <div className="results">
+            <ul>
+                <li>result1</li>
+                <li>result2</li>
+                <li>result3</li>
+                <li>result4</li>
+                <li>result5</li>
+                <li>result6</li>
+                <li>result7</li>
+                <li>result8</li>
+            </ul>
+        </div>
+    )
+}
